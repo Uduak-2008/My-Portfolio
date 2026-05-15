76e00aae1ee0518bc85659bd816168d1505d6246
Uduak-2008 <uduakudoma2008@gmail.com>
2026-05-15 10:26:38 +0100

    Update style.css

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ menu.classList.add('show');
 menuBtn.classList.add('hide');
 };
 document.onclick = function(event) {
-  if (menu.classList.contains('show') && event.target !== menuBtn) {
+  if (menu.classList.contains('show') && event.target !== menuToggle && !menu.contains(event.target)) {
     menu.classList.remove('show');
     menuBtn.classList.remove('hide');
   }
