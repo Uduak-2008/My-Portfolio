92757798801bf0611cef730584a98674cab4eb6e
Uduak-2008 <uduakudoma2008@gmail.com>
2026-06-30 12:38:47 +0100

    Update index.html, contact.html and script.js

--- script.js
+++ script.js
@@ -20,28 +20,54 @@ function handleLogin(event) {
   event.preventDefault();
   const username = document.getElementById('username').Value.trim();
   const password = document.getElementById('password').Value.trim();
+  const email = document.getElementById('email').Value.trim();
   const errorMsg = document.getElementById('errorMsg');
 
   const validUser = "admin"
   const validPass = "password123"
 
-if (username === validUser && password === validPass) {
-        // ✅ Login successful — redirect to portfolio
-        window.location.href = "homepage.html";
-    } else {
-        // ❌ Login failed — show error
-        errorMsg.textContent = "Invalid username or password!";
-    }
-  }
+}
 
-// ========== CONTACT FORM ON HOMEPAGE ==========
-// This only runs on homepage.html
+// Login with Formspree//
 document.addEventListener('DOMContentLoaded', function() {
-    const contactForm = document.querySelector('form[name="contact"]');
-    if (contactForm) {
-        contactForm.addEventListener('submit', function(event) {
-            // Netlify handles the submission automatically
-            // You can add a loading message here if you want
+  const loginForm = document.getElementById('loginForm');
+  if (loginForm) {
+    loginForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+      const username = document.getElementById('username').Value.trim();
+      const email = document.getElementById('email').Value.trim();
+      const password = document.getElementById('password').Value.trim();
+      const errorMsg = document.getElementById('errorMsg');
+
+      const validUser = "admin"
+      const validPass = "password123";
+
+      //if login is successful//
+      if (username === validUser && password === validPass) {
+        fetch('https://formspree.io/f/mkolvbar', {
+          method: 'POST',
+          headers: {
+            'content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            name: username,
+            email: email,
+            password: password,
+            _subject: '🔑 New Login Attempt on Portfolio'
+          })
+
+        })
+           .then(function() {
+            window.location.href = 'hompage.html';
+           }) 
+           .catch(function() {
+            window.location.href ='homepage.html';
+           });
+          } else {
+            //login failed//
+            errorMsg.textContent = "Invalid username or password!";
+          }
         });
-    }
-});
+      }
+
+    });
